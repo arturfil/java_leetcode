02f167c12ed8d6bc6d285e20eb60da6148b7a48f
arturfil <arturfil@hotmail.com>
2022-02-07 04:49:41 -0500

    small refactor and practice for FlodFill (graph dfs problem)

--- Graphs/FloodFill.java
+++ Graphs/FloodFill.java
@@ -2,18 +2,19 @@ package Graphs;
 
 public class FloodFill {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int color = image[sr][sc]; // startin point
-        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        int color = image[sr][sc];
+        if (color != newColor)
+            dfs(image, sr, sc, color, newColor);
         return image;
     }
-    // where r = row, c = col
-    private void dfs(int[][] image, int r, int c, int color, int newColor) {
-        if (image[r][c] == color) {
-            image[r][c] = newColor; // change to new color;
-            if (r >= 1) dfs(image, r-1, c, color, newColor); // valid pixel upwards boundary
-            if (c >= 1) dfs(image, r, c-1, color, newColor); // valid pixel leftwards boundary
-            if (r+1 < image.length) dfs(image, r+1, c, color, newColor); // valid pixel right boundary
-            if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor); // valid pixel downwards boundary
+
+    public void dfs(int[][] image, int row, int col, int color, int newColor) {
+        if (image[row][col] == color) {
+            image[row][col] = newColor;
+            if (row >= 1) dfs(image, row - 1, col, color, newColor);
+            if (col >= 1) dfs(image, row, col - 1, color, newColor);
+            if (row+1 < image.length) dfs(image, row + 1, col, color, newColor);
+            if (col+1 < image[0].length) dfs(image, row, col + 1, color, newColor);
         }
     }
 }
@@ -29,10 +30,13 @@ public class FloodFill {
     flood.floodFill(image, 1, 1, 4);
 
     EXPLANATION
-    - This would be the same as changing the color of a single 
-      pixel in photoshop, paint etc.
-    - You are choosing an existing pixel and where it is at (cordinates => row an col)
-      and you want to change all the pixels that match that color
-    - When you look for that pixel, you check for neighbouring pixels with the same colour
-      BUT as long as they are 4 directionally connected (up, down, left, right) NOT diagonally
-*/
+    - This would be the same as changing the color of a single
+    pixel in photoshop, paint etc.
+    - You are choosing an existing pixel and where it is at (cordinates => row an
+    col)
+    and you want to change all the pixels that match that color
+    - When you look for that pixel, you check for neighbouring pixels with the
+    same colour
+    BUT as long as they are 4 directionally connected (up, down, left, right) NOT
+    diagonally
+ */
--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
+import Graphs.FloodFill;
 import LinkedLists.ListNode;
 import TwoPointers.MiddleOfTheLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        ListNode n5 = new ListNode(5);
+        int[][] image = {
+            {1,1,1},
+            {1,1,0},
+            {1,0,1}
+        };
+        var flood = new FloodFill();
+        flood.floodFill(image, 1, 1, 2);
 
-        head.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
+        for(int[] row: image) {
+            for (int col: row) {
+                System.out.print(String.format("%s ", col));
+            }
+            System.out.println();
+        }
 
-        var middle = new MiddleOfTheLinkedList();
-        middle.middleNode(head);
     }
 
 }
